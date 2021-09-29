5f8db2c8093470b55e30950e0dc8e76eedad6b3f
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-09-29 00:58:28 -0700

    Cache Random.

--- voice mod test/Extensions/Extensions.cs
+++ voice mod test/Extensions/Extensions.cs
@@ -6,12 +6,23 @@ namespace SlapCityVoiceMod.Extensions
 {
     static class Extensions
     {
+        private static Random _random;
+        private static Random Random
+        {
+            get
+            {
+                _random ??= new Random(DateTime.Now.Millisecond);
+                return _random;
+            }
+            set => _random = value;
+        }
+
         public static T GetRandomItem<T>(this IEnumerable<T> itemsEnumerable, Func<T, float> weightKey)
         {
             var items = itemsEnumerable.ToList();
 
             var totalWeight = items.Sum(x => weightKey(x));
-            var randomWeightedIndex = (float) new Random(DateTime.Now.Millisecond).NextDouble() * totalWeight;
+            var randomWeightedIndex = (float)Random.NextDouble() * totalWeight;
             var itemWeightedIndex = 0f;
             foreach (var item in items)
             {
